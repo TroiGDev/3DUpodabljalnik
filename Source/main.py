b18844deafa481c22fc88d3c13b7bcb41c0b28fe
Troi <143833120+TroiGDev@users.noreply.github.com>
2025-02-16 10:28:33 +0100

    Updated main.py
    removed pyramids, added in a cube, added in camera movement and set center of rotation to camera

--- Source/main.py
+++ Source/main.py
@@ -26,9 +26,6 @@ class Face:
         self.depth = (self.point1[2] + self.point2[2] + self.point3[2]) / 3
 
     def draw(self):
-        #recalculate depth
-        self.depth = (self.point1[2] + self.point2[2] + self.point3[2]) / 3
-
         #get projected points
         pp1 = project(self.point1, FOV)
         pp2 = project(self.point2, FOV)
@@ -107,6 +104,11 @@ class Face:
         self.point1 = rotate_point(self.point1)
         self.point2 = rotate_point(self.point2)
         self.point3 = rotate_point(self.point3)
+    
+    def movePoints(self, speedX, speedY, speedZ):
+        self.point1 = (self.point1[0] + speedX, self.point1[1] + speedY, self.point1[2] + speedZ)
+        self.point2 = (self.point2[0] + speedX, self.point2[1] + speedY, self.point2[2] + speedZ)
+        self.point3 = (self.point3[0] + speedX, self.point3[1] + speedY, self.point3[2] + speedZ)
 
 def project(point, FOV):
     x, y, z = point
@@ -116,60 +118,56 @@ def project(point, FOV):
     # Add screen center offset
     return (screen_x + screenWidth/2, screen_y + screenHeight/2)
 
-########################################################################################
+###################################################################################################################################
+#camera position  (center of rotation for the camera)
 baseX = 0
-baseY = 100
-baseZ = 500
+baseY = 0
+baseZ = 0
 
-#get all faces
-faces = []
-
-gofrickyourself       #Forced error for todo reminder, create movement control by moving every face in opposite direction
-
-#initialize 2 pyramids
-pyramid_vertices = [
-    (-100, 0, -100),   # bl
-    (100, 0, -100),    # br
-    (100, 0, 100),     # tr
-    (-100, 0, 100),    # tl
-    (0, -200, 0)        # top
-]
-
-pyramid_vertices1 = [
-    (-100 - 250, 0, -100),   # bl
-    (100 - 250, 0, -100),    # br
-    (100 - 250, 0, 100),     # tr
-    (-100 - 250, 0, 100),    # tl
-    (0 - 250, -300, 0)        # top
-]
-
-faces = [
-    #first pyramid
-    Face((255, 0, 0), pyramid_vertices1[0], pyramid_vertices1[1], pyramid_vertices1[4]),
-    Face((0, 255, 0), pyramid_vertices1[1], pyramid_vertices1[2], pyramid_vertices1[4]),
-    Face((0, 0, 255), pyramid_vertices1[2], pyramid_vertices1[3], pyramid_vertices1[4]),
-    Face((255, 255, 0), pyramid_vertices1[3], pyramid_vertices1[0], pyramid_vertices1[4]),
-
-    #bottom square
-    Face((255, 0, 255), pyramid_vertices1[3], pyramid_vertices1[2], pyramid_vertices1[1]),
-    Face((255, 0, 255), pyramid_vertices1[3], pyramid_vertices1[0], pyramid_vertices1[1]),
-
-    #second pyramid
-    Face((255, 0, 255), pyramid_vertices[0], pyramid_vertices[1], pyramid_vertices[4]),
-    Face((0, 255, 255), pyramid_vertices[1], pyramid_vertices[2], pyramid_vertices[4]),
-    Face((255, 255, 0), pyramid_vertices[2], pyramid_vertices[3], pyramid_vertices[4]),
-    Face((255, 0, 0), pyramid_vertices[3], pyramid_vertices[0], pyramid_vertices[4]),
-
-    #bottom square
-    Face((0, 0, 255), pyramid_vertices[3], pyramid_vertices[2], pyramid_vertices[1]),
-    Face((0, 0, 255), pyramid_vertices[3], pyramid_vertices[0], pyramid_vertices[1])
-]
-
-########################################################################################
-
-FOV = 90
+FOV = 220          #optimal starting fov
 zoom = 5
 rotationSpeed = 0.05
+movementSpeed = 0.20
+
+#get all faces and bodies
+faces = []
+
+cubeX = 0
+cubeY = 250
+cubeZ = 250
+
+cubeVertz=[
+    (cubeX -100, cubeY -100, cubeZ -100),      #top    #dl     #0
+    (cubeX -100, cubeY -100, cubeZ +100),               #tl     #1
+    (cubeX +100, cubeY -100, cubeZ +100),                #tr     #2
+    (cubeX +100, cubeY -100, cubeZ -100),               #dr     #3
+
+    (cubeX -100, cubeY +100, cubeZ -100),       #bottom #dl     #4
+    (cubeX -100, cubeY +100, cubeZ +100),                #tl     #5
+    (cubeX +100, cubeY +100, cubeZ +100),                 #tr     #6
+    (cubeX +100, cubeY +100, cubeZ -100)                #dr     #7
+]
+
+#cube faces
+face = Face((255, 0, 0), cubeVertz[5], cubeVertz[4], cubeVertz[7])  #top
+face = Face((255, 0, 0), cubeVertz[5], cubeVertz[6], cubeVertz[7])
+
+face = Face((0, 0, 255), cubeVertz[1], cubeVertz[0], cubeVertz[3])  #bottom
+face = Face((0, 0, 255), cubeVertz[1], cubeVertz[2], cubeVertz[3])
+
+face = Face((255, 255, 0), cubeVertz[1], cubeVertz[0], cubeVertz[4])    #left
+face = Face((255, 255, 0), cubeVertz[1], cubeVertz[5], cubeVertz[4])
+
+face = Face((0, 255, 255), cubeVertz[3], cubeVertz[2], cubeVertz[6])   #right
+face = Face((0, 255, 255), cubeVertz[3], cubeVertz[7], cubeVertz[6])
+
+face = Face((0, 255, 0), cubeVertz[1], cubeVertz[2], cubeVertz[6]) #back
+face = Face((0, 255, 0), cubeVertz[1], cubeVertz[5], cubeVertz[6])
+
+face = Face((255, 0, 255), cubeVertz[0], cubeVertz[3], cubeVertz[7]) #front
+face = Face((255, 0, 255), cubeVertz[0], cubeVertz[4], cubeVertz[7])
+
+###################################################################################################################################
 
 while True:
     for event in pygame.event.get():
@@ -204,10 +202,31 @@ while True:
         if keys[pygame.K_RIGHT]:
             face.rotate_y(rotationSpeed)
 
+        #movement
+        if keys[pygame.K_w]:
+            face.movePoints(0, 0, -movementSpeed)
+        if keys[pygame.K_s]:
+            face.movePoints(0, 0, movementSpeed)
+
+        if keys[pygame.K_a]:
+            face.movePoints(movementSpeed, 0, 0)
+        if keys[pygame.K_d]:
+            face.movePoints(-movementSpeed, 0, 0)
+
+        if keys[pygame.K_SPACE]:
+            face.movePoints(0, movementSpeed, 0)
+        if keys[pygame.K_LSHIFT]:
+            face.movePoints(0, -movementSpeed, 0)
+
     screen.fill((18, 18, 18))
 
     #update faces, sorted by distance from screen, to draw closer faces on top
     for face in sorted(faces, key=lambda x: x.depth, reverse=True):
+
+        #recalculate facedepth
+        face.depth = (face.point1[2] + face.point2[2] + face.point3[2]) / 3
+
+        #draw when in front
         if face.depth > 0:
             face.draw()
 
